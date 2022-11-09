ded93054c97aad06aec935fcaeeab156d3d38299
Prof.G <csprojacct@gmail.com>
2022-11-09 14:54:24 -0500

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//Print Statements
+//console.log("Hello World");
+//console.log("Hi");
+
+//window.alert("I like Joel Embiid");
+
+//let firstName = "Luis";
+//let age = 30;
+
+//let student = false; 
+//age = age + 1;
+//console.log(age);
+//console.log("Hello"+ firstName);
+//console.log(student);
+
+//document.getElementById("p1").innerHTML = "Hello " + firstName;
+//document.getElementById("p2").innerHTML = "Age:  " + age;
+//document.getElementById("p3").innerHTML = "Are you a  " + student;
+
+
+//let students = 20;
+//students *=2;
+//console.log(students);
+
+//let username = window.prompt("What's your name?");
+//console.log(username);
